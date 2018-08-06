2edf34c99adb11d2562e1af761727bd253f2310f
derekfogge <dfogge@me.com>
2018-08-05 22:08:58 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var url = "https://api.nasa.gov/planetary/apod?api_key=e0iOm38ojRgMvysE95lpWe2PCnsEo9OOVUkZRnMP";
+
+$.ajax({
+  url: url,
+  success: function(result) {
+    $("#nasa").attr("src", result.url);
+  }
+});
