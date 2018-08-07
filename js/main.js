fc45d1ed0dc87da53770fe547e3518e59726222e
derekfogge <dfogge@me.com>
2018-08-07 19:49:12 -0400

    allow scrolling layout

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 function nasaBG() {
   var url = "https://api.nasa.gov/planetary/apod?api_key=e0iOm38ojRgMvysE95lpWe2PCnsEo9OOVUkZRnMP";
-  var image = $("#nasa");
+  var image = $(".hi");
   $.ajax({
     url: url,
     success: function(result) {
-      image.attr("src", result.url);
+      image.css('background-image', 'url(' + result.url + ')');
     }
   });
 }
 nasaBG();
 
-// function hi() {
-//   $(window).on('click keypress wheel', function() {
-//     $('.hi').stop().fadeOut('slow');
-//     $('.hey').delay('600')
-//       .css('visibility','visible')
-//       .hide().fadeIn('slow');
-//   })
-// };
-// hi();
+
+$(window).resize(function() {
+    $('.hi').height($(window).height() / 1);
+    $('.hi h1').css('padding-top', $(window).height() / 2.5);
+}).resize();
+
